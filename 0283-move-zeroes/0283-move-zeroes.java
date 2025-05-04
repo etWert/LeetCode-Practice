845b3991320c6421f5ed51a43d1153a3fcf4e1be
Iti Donat <162367920+etWert@users.noreply.github.com>
2025-05-04 10:51:21 +0300

    Time: 36 ms (8.36%), Space: 46.2 MB (32.33%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -2,12 +2,11 @@ class Solution {
     public void moveZeroes(int[] nums) {
         for(int i=0;i<nums.length-1;i++){
             if(nums[i]==0){
-                for(int j=i+1;j<nums.length;j++){
-                    if(nums[j]!=0){
-                        nums[i]=nums[j];
-                        nums[j]=0;
-                        break;
-                    }
+                int j;
+                for(j=i+1;j<nums.length&&nums[j]==0;j++);
+                if(j<nums.length){
+                    nums[i]=nums[j];
+                    nums[j]=0;
                 }
             }
         }
