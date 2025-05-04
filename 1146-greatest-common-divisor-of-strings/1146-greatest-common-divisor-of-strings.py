c8b3691004392b98d1e9022c87baedcc1862acf9
Iti Donat <162367920+etWert@users.noreply.github.com>
2025-05-04 13:12:57 +0300

    Time: 0 ms (100%), Space: 17.9 MB (25.37%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        if str1+str2 != str2+str1:
+            return ""
+        else:
+            gcd_len = gcd(len(str1), len(str2))
+            return str1[:gcd_len]
